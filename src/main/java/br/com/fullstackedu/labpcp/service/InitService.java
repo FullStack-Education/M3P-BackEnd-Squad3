b53a7c6d29a145b2f00ca3a6e06c1e40dc4fc79a
Leandro Dias <lvd@certi.org.br>
2024-10-26 02:18:13 -0300

    feat: adicionado mock de professores

---
+++ src/main/java/br/com/fullstackedu/labpcp/service/InitService.java
@@ -0,0 +1,64 @@
+package br.com.fullstackedu.labpcp.service;
+
+import br.com.fullstackedu.labpcp.database.entity.UsuarioEntity;
+import br.com.fullstackedu.labpcp.database.repository.PapelRepository;
+import br.com.fullstackedu.labpcp.database.repository.UsuarioRepository;
+import jakarta.annotation.PostConstruct;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class InitService {
+
+    private final UsuarioRepository usuarioRepository;
+    private final PapelRepository papelRepository;
+    private final BCryptPasswordEncoder bCryptEncoder;
+
+    public InitService(UsuarioRepository usuarioRepository, PapelRepository papelRepository, BCryptPasswordEncoder bCryptEncoder) {
+        this.usuarioRepository = usuarioRepository;
+        this.papelRepository = papelRepository;
+        this.bCryptEncoder = bCryptEncoder;
+    }
+
+    private void insertIfNotExistsUsuarioEntity(Long id, String login, String senha, String papel) throws Exception{
+        if (usuarioRepository.findByLogin(login).isEmpty()) {
+            log.info("InitService -> Inserindo o usuário [{}] ",login);
+            UsuarioEntity newUser = new UsuarioEntity();
+            newUser.setId(id);
+            newUser.setLogin(login);
+            newUser.setSenha(bCryptEncoder.encode(senha));
+            newUser.setNome(login);
+            newUser.setPapel(papelRepository.findByNome(papel)
+                    .orElseThrow(
+                            () -> new RuntimeException("Erro ao inserir usuario inicial. O Papel ["+ login + "] não foi encontrado" )
+                    )
+            );
+            usuarioRepository.save(newUser);
+        }
+    }
+
+    @PostConstruct
+    public void initUsuarios() throws Exception {
+        log.info("InitService -> Verificando necessidade de inserir usuários iniciais ");
+        Long id = 1L;
+        insertIfNotExistsUsuarioEntity(id++, "ADM", "ADM","ADM");
+        insertIfNotExistsUsuarioEntity(id++, "admin@mail.com", "admin","ADM");
+        //TODO encapsule the users bellow to env "test"
+        insertIfNotExistsUsuarioEntity(id++, "pedagogico@mail.com", "pedagogico","PEDAGOGICO" );
+        insertIfNotExistsUsuarioEntity(id++, "recruiter@mail.com", "recruiter","RECRUITER" );
+        insertIfNotExistsUsuarioEntity(id++, "professor@mail.com", "professor","PROFESSOR" );
+        insertIfNotExistsUsuarioEntity(id++, "aluno@mail.com", "aluno","ALUNO" );
+
+        //professores
+        log.info("InitService -> Cria profesores");
+        Long[] idProfessores = new Long[]{id++,id++,id++,id++,id++,id++};
+        insertIfNotExistsUsuarioEntity(idProfessores[0], "eduardo@mail.com", "professor","PROFESSOR" );
+        insertIfNotExistsUsuarioEntity(idProfessores[1], "gabriel@mail.com", "professor","PROFESSOR" );
+        insertIfNotExistsUsuarioEntity(idProfessores[2], "ray@mail.com", "professor","PROFESSOR" );
+        insertIfNotExistsUsuarioEntity(idProfessores[3], "joaoPaulo@mail.com", "professor","PROFESSOR" );
+        insertIfNotExistsUsuarioEntity(idProfessores[4], "duda@mail.com", "professor","PROFESSOR" );
+
+    }
+}
